8265339712f5a89cecd68792f96371add3222eea
myeongsu0923 <dieseltg1@daegu.ac.kr>
2022-04-29 21:56:49 +0900

    added "cityNumber.csv" and modified "naverWeather.py, test.py"

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 from naverWeather import *
-l = naverWeather.map_cityNum.keys()
-print(naverWeather().getWeather())
-for s in l :
-    print(naverWeather(s).getWeather())
+
+print("<오늘의 날씨>")
+default = naverWeather('서울')
+print(default.getWeather())
+
+while True:  
+    print("\n날씨를 알고 싶은 도시명을 입력하세요(ex: 서울, 제주) : ", end="")
+
+    city = input()
+    if city == "stop":
+        break
+    if city not in naverWeather.map_cityNum:
+        print("잘못된 도시명입니다.")
+        continue
+    temp = naverWeather(city)
+    print(temp.getWeather())
