0dda0b5ebf6453b014b5dc0dde48793987dac4ab
myeongsu0923 <dieseltg1@daegu.ac.kr>
2022-04-30 18:55:51 +0900

    Modified test.py

--- test.py
+++ test.py
@@ -1,17 +1,23 @@
 from naverWeather import *
 
-print("<오늘의 날씨>")
-default = naverWeather('서울')
-print(default.getWeather())
-
 while True:  
     print("\n날씨를 알고 싶은 도시명을 입력하세요(ex: 서울, 제주) : ", end="")
-
+    print("\nq를 누르면 종료합니다.")
+    print(">> ", end="")
     city = input()
-    if city == "stop":
+    print("\n")
+
+    if city == "q":
+        print("종료.")
         break
     if city not in naverWeather.map_cityNum:
         print("잘못된 도시명입니다.")
         continue
+
     temp = naverWeather(city)
+    
     print(temp.getWeather())
+    print("====== 오늘 기온에 맞는 옷 추천 ======")
+    for fits in temp.getFits():
+        print(fits.replace('"','').strip())
+
